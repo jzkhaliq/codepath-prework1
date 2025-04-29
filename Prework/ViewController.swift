ab8aaf5af5dc04808c4f12c29915a8bf3a2d1f7d
jzkhaliq <jehanzaib.khaliq@gmail.com>
2025-04-29 18:23:16 -0500

    Added prework files to rep

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeBackgroundColor: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Any setup after loading the view can go here
     }
 
+    @IBAction func changeBackgroundColorTapped(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
 
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
 }
-
